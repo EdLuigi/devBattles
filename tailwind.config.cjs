ad473e5a372d94aac3632840d2d841eaedf59db7
Luigi <luigi.ciuffi@hotmail.com>
2022-09-20 19:06:09 -0300

    config cores geral

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: "#101018",
+                secondary: "#1F1E2C",
+                purple: "#6366F1",
+            },
+        },
     },
     plugins: [],
 };
