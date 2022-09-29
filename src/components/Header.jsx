2e798ed8e3696dd999daf5f8187a5daa503ef6e0
Luigi <luigi.ciuffi@hotmail.com>
2022-09-29 20:35:29 -0300

    adicionado feather icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,25 @@
 import React from "react";
-import { FiBell } from "react-icons/fi";
-import { AiOutlinePlus } from "react-icons/ai";
-import { MdKeyboardArrowDown } from "react-icons/md";
+import { Bell, ChevronDown, Plus } from "react-feather";
 
-const iconSize = "20px";
-
-const Bell = (props) => {
+const BellIcon = (props) => {
     return (
-        <a href="#" className="flex relative">
-            <FiBell className="text-white" size={iconSize}></FiBell>
+        <a href="#" className="flex relative mt-[3px]">
+            <Bell className="text-white" size={"20px"} />
             <BlueCircle />
         </a>
     );
 };
 
-const Plus = (props) => {
+const PlusIcon = (props) => {
     return (
         <div className="flex ">
-            <a href="#" className="flex">
-                <AiOutlinePlus className="text-white" size={iconSize} />
-                <MdKeyboardArrowDown className="text-white ml-[-2px] mt-[2px]" />
+            <a href="#" className="flex items-center ">
+                <Plus className="text-white" size={"23px"} />
+                <ChevronDown
+                    className="text-white"
+                    size={"14px"}
+                    strokeWidth={"3px"}
+                />
             </a>
         </div>
     );
@@ -38,7 +38,7 @@ const User = (props) => {
 const BlueCircle = (props) => {
     return (
         <div className="w-full absolute flex justify-end items-start mt-[-2px] mx-[2px]">
-            <div className="h-3 w-3 bg-[#37AADF] rounded-xl border-[1px] border-secondary "></div>
+            <div className="h-3 w-3 bg-linear rounded-xl border-[1px] border-secondary "></div>
         </div>
     );
 };
@@ -46,13 +46,13 @@ const BlueCircle = (props) => {
 export default function Header() {
     return (
         <div>
-            <div className="flex bg-secondary px-14 py-6 justify-between">
+            <div className="flex bg-secondary px-14 py-6 justify-between ">
                 <a href="#" className="text-white font-medium">
                     DevBattles
                 </a>
-                <div className="flex pt-[2px] gap-4">
-                    <Bell />
-                    <Plus />
+                <div className="flex pt-[2px] gap-3">
+                    <BellIcon />
+                    <PlusIcon />
                     <User />
                 </div>
             </div>
