a80ab98888fe94990892033e4ce2c544099aadee
Luigi <luigi.ciuffi@hotmail.com>
2022-10-14 19:36:38 -0300

    estilos prontos em BioCard2

--- src/components/Profile/Bio/BioCard2.jsx
+++ src/components/Profile/Bio/BioCard2.jsx
@@ -1,26 +1,33 @@
 import React from "react";
 import { user } from "../../../Utils/MockData";
 
+const Container = (props) => {
+    return (
+        <div className="w-[210px] h-[210px] bg-secondary rounded-md text-text_primary flex flex-wrap gap-4 p-4">
+            {props.children}
+        </div>
+    );
+};
+
+const SmallBox = (props) => {
+    return (
+        <div className="bg-background flex flex-col h-20 w-20 rounded-md justify-center items-center gap-1">
+            <p className="text-t1 font-light">
+                {props.title + (props.data != 1 ? "s" : "")}
+            </p>
+            <p className="text-t4 font-bold">{props.data}</p>
+        </div>
+    );
+};
+
 export default function BioCard2(props) {
     const userData = user[0];
     return (
-        <div className="w-[210px] h-[210px] bg-secondary rounded-md text-text_primary flex flex-wrap gap-1 p-1">
-            <div className="bg-background flex flex-col h-24 w-24">
-                <p>Pendentes</p>
-                <p>{userData.pendentes}</p>
-            </div>
-            <div className="bg-background flex flex-col h-24 w-24">
-                <p>Favoritados</p>
-                <p>{userData.favoritados}</p>
-            </div>
-            <div className="bg-background flex flex-col h-24 w-24">
-                <p>Criados</p>
-                <p>{userData.criados}</p>
-            </div>
-            <div className="bg-background flex flex-col h-24 w-24">
-                <p>Submetidos</p>
-                <p>{userData.submetidos}</p>
-            </div>
-        </div>
+        <Container>
+            <SmallBox title="Pendente" data={userData.pendentes} />
+            <SmallBox title="Favoritado" data={userData.favoritados} />
+            <SmallBox title="Criado" data={userData.criados} />
+            <SmallBox title="Submetido" data={userData.submetidos} />
+        </Container>
     );
 }
